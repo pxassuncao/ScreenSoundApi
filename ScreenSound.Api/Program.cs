d7f71c085f6424a92a1f49c9f1f57b570d85120f
pxassuncao <mirassuncaobjj07@gmail.com>
2024-04-26 18:06:39 -0300

    Melhorando MetotodoArtistaNome

--- ScreenSound.Api/Program.cs
+++ ScreenSound.Api/Program.cs
@@ -8,10 +8,26 @@ builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>
     = ReferenceHandler.IgnoreCycles);
 var app = builder.Build();
 
-app.MapGet("/", () =>
+app.MapGet("/Artistas", () =>
 {
     var dal = new DAL<Artista>(new ScreenSoundContext());
-    return dal.Listar();
+    return Results.Ok(dal.Listar());
+});
+
+app.MapGet("/Artistas/{nome}", (string nome) =>
+{
+    if (nome == null)
+    {
+        // Retornar um resultado de erro, por exemplo, BadRequest
+        return Results.BadRequest("O parâmetro 'nome' não pode ser nulo.");
+    }
+    var dal = new DAL<Artista>(new ScreenSoundContext());
+    var artista =  dal.RecuperarPor(a=> a.Nome.ToUpper().Equals(nome.ToUpper()));
+    if(artista is null)
+    {
+        return Results.NotFound();
+    }
+    return Results.Ok(artista);
 });
 
 app.Run();
